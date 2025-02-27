abb37dd00eb169efae291bb6ccbad40b329a24f2
Ishmaam Iftekhar Khan <ishmaam@iut-dhaka.edu>
2025-02-27 20:57:40 +0600

    Footer Done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Sidebar from './components/Sidebar'
 import "./index.css"
+import Footer from './components/Footer'
 function App() {
   
 
@@ -8,6 +9,7 @@ function App() {
     <>
     <div className='App'>
       <Sidebar/>
+      <Footer/>
       </div>
     </>
   )
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import "../App.css"
+
+import Tools from '../components/Tools'
+const Footer = () => {
+    return (
+        <>
+            <div className="footer">
+                <p>©2025 Reyansh. All rights reserved.</p>
+            </div>
+        </>
+    )
+}
+
+export default Footer
